f5eb3064562b9aa5ef1af398b8d6b84a9736d71d
student_2333 <lgc2333@126.com>
2023-03-06 02:46:27 +0800

    why not base64

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { segment } = require("oicq")
 const winston = require('winston');
 const dayjs = require('dayjs');
 const  exists = fs.existsSync;
+const { readFileSync } = require('fs')
 const { GroupMessageEvent, FriendMessageEvent, GroupLeftEvent, GroupJoinEvent } = require("./msgBuilder");
 let today = dayjs();
 class Adapter {
@@ -209,13 +210,9 @@ class Adapter {
             case 'oicq':
                 return segment.image(file);
             case 'gocq':
-                if(exists(file)){
-                    return { type: 'image', data: { file:'file:///'+file } }
-                }else{
-                    return { type: 'image', data: { file:file, subType:0} }
-                    // 暂时只能使用file进行发送
-                    // 详见https://github.com/Mrs4s/go-cqhttp/issues/1660
-                }
+                if (typeof file === 'string' && exists(file)) file = readFileSync(file)
+                if (file instanceof Buffer) file = `base64://${file.toString('base64')}`
+                return { type: 'image', data: { file: file, subType: 0 } }
         }
     }
     at(qid) {
